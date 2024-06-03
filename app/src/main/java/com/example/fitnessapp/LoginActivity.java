854c1c7896cb72f758f2704fef0ee041d1d1e592
ilyount02 <qkddlfdud02@gmail.com>
2024-06-03 23:56:14 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fitnessapp/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.fitnessapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity{
+    private Button LoginBtn, RegisterBtn;
+    private EditText loginid, loginpasswd;
+    private CheckBox setid;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        LoginBtn = (Button) findViewById(R.id.LoginBtn);
+        RegisterBtn = (Button) findViewById(R.id.RegisterBtn);
+        loginid = (EditText) findViewById(R.id.loginid);
+        loginpasswd = (EditText) findViewById(R.id.loginpasswd);
+        setid = (CheckBox) findViewById(R.id.setid);
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginid.getText().toString().trim();
+                String pwd = loginpasswd.getText().toString().trim();
+            }
+        });
+
+    }
+}
