d72094580aafb3d0c448fb67eeb24298988dbcb7
ilyount02 <qkddlfdud02@gmail.com>
2024-06-04 21:05:54 +0900

    ui

---
+++ app/src/main/java/com/example/fitnessapp/MapActivity.java
@@ -0,0 +1,12 @@
+package com.example.fitnessapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
